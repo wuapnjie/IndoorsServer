a646d9ffd158d1e28fe5f6b2bfc1bd3d9336d88b
wuapnjie <wupanjie0611@gmail.com>
2018-05-04 15:13:15 +0800

    WKNN -> KNN

--- src/main/kotlin/com/indoors/common/KNN.kt
+++ src/main/kotlin/com/indoors/common/KNN.kt
@@ -12,13 +12,11 @@ import kotlin.math.sqrt
  */
 
 fun Room.computePosition(wifiList: List<WiFiInfo>, K: Int = 4): Position {
-//  val distances = arrayListOf<Double>()
 
   val positionsWithDistance = arrayListOf<Pair<RoomPosition, Double>>()
   // 计算所有的距离
   this.positions.forEach {
     val distance = wifiList to it
-//    distances += distance
     positionsWithDistance += it to distance
   }
 
@@ -27,33 +25,30 @@ fun Room.computePosition(wifiList: List<WiFiInfo>, K: Int = 4): Position {
     o1.second.compareTo(o2.second)
   })
 
-  // 对距离进行排序
-//  distances.sort()
-
-  val k = if (K < positionsWithDistance.size) K else positionsWithDistance.size
-
-  // 前k个1/distance的和
-  val d = (0 until k).sumByDouble { 1 / positionsWithDistance[it].second }
-
-  // 选出前k个邻近向量计算权值w
-  val w = arrayListOf<Double>()
-  for (i in 0 until k) {
-    w += (1 / positionsWithDistance[i].second) / d
-  }
-
-  // 计算出加权位置
-  var x = 0.0
-  var y = 0.0
-
-  for (i in 0 until k) {
-    x += w[i] * positionsWithDistance[i].first.x
-    y += w[i] * positionsWithDistance[i].first.y
-  }
-
-  return Position(x, y)
+  return Position(positionsWithDistance[0].first.x, positionsWithDistance[1].first.y)
+//  val k = if (K < positionsWithDistance.size) K else positionsWithDistance.size
+//
+//  // 前k个1/distance的和
+//  val d = (0 until k).sumByDouble { 1 / positionsWithDistance[it].second }
+//
+//  // 选出前k个邻近向量计算权值w
+//  val w = arrayListOf<Double>()
+//  for (i in 0 until k) {
+//    w += (1 / positionsWithDistance[i].second) / d
+//  }
+//
+//  // 计算出加权位置
+//  var x = 0.0
+//  var y = 0.0
+//
+//  for (i in 0 until k) {
+//    x += w[i] * positionsWithDistance[i].first.x
+//    y += w[i] * positionsWithDistance[i].first.y
+//  }
+//
+//  return Position(x, y)
 }
 
-//
 // 计算当前WiFi信息与房间基准点WiFi信息之间的欧式距离
 infix fun List<WiFiInfo>.to(other: RoomPosition): Double {
   val otherRSSIMap = other.macRSSIMap
